dd0ef90094002b6f16599a7b5a301631263036c8
Pascual <pascual.vila.segura@hotmail.com>
2019-02-19 16:04:29 +0000

    second commit

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # WDI-Project-1
+https://pascual143.github.io/WDI-Project-1/
---
+++ scripts/main.js
@@ -0,0 +1,247 @@
+// // const player = playerOne
+//
+// //
+// // const grid = function() {
+// //   for (let i = 0; i < length + 7; i++) {
+// //     grid[i]
+// //   }
+// // }
+//
+// // const namePlayerOne = prompt()
+// // function welcome(){
+// //   const name = document.getElementById('welcome')
+// //   if (name.value === ''){
+// //     return true
+// //   } else {
+// //     return false
+// //   }
+// // }
+// // if (namePlayerOne === true){
+// //   alert('we need your name to start')
+// //
+// //   document.write('welcome')
+// //   welcome()
+// // }
+//
+// // function myFunction() {
+// //   var x = document.getElementById('myText').name
+// //   document.getElementById('name').innerHTML = x
+// // }
+// // myFunction()
+// // document.getElementById('name').innerHTML = welcome
+//
+//
+//
+// //function change(){
+// //FUNCTION CHOOSE coins
+// //buttons
+// var coinsGold = gold
+// var coinsPurple = purple
+//
+//
+// //FUNCTION PLAYERONE
+// var coins = gold
+// var coins = purple
+// function playerOne(){
+//   if (playerOne === gold) {
+//     return true
+//   } else {
+//     return false
+//   }
+// }
+// playerOne()
+//
+//
+// }
+// //FUNCTION PLAYERTWO
+// function playerTwo(){
+//   if (playerTwo === purple) {
+//     return true
+//   } else {
+//     return false
+//   }
+// }
+// playerTwo()
+// //FUNCTION PLAYERMACHINE
+// // const mac = Math.floor(Math.random())
+// //FUNCTION IS FULL
+//
+//
+// function option (human) {
+//
+//   const array = [
+//
+//     [0,1,2,3,4,5,6],
+//     [7,8,9,10,11,12,13],
+//     [14,15,16,17,18,19,20],
+//     [21,22,23,24,25,26,27],
+//     [28,29,30,31,32,33,34],
+//     [35,36,37,38,39,40,41]
+//   ]
+//
+// }
+//
+//   //FUNCTION 4 IN A ROW(MAKE THE ROWS)
+//   const wins = [
+//     //VERTICALLY
+//     wins[0] = new array[0, 7, 14, 21, 28, 35],
+//     wins[1] = new array[1, 8, 15, 22, 29, 36],
+//     wins[2] = new array[2, 9, 16, 23, 30, 37],
+//     wins[3] = new array[3, 10, 17, 24, 31, 38],
+//     wins[4] = new array[4, 11, 18, 25, 32, 39],
+//     wins[5] = new array[5, 12, 19, 26, 33, 40],
+//     wins[6] = new array[6, 13, 20, 27, 34, 41],
+//
+//
+//     //HORIZONTALY
+//
+//     wins[7] = new array[0,1,2,3,4,5,6],
+//     wins[8] = new array[7,8,9,10,11,12,13],
+//     wins[11] = new array[14,15,16,17,18,19,20],
+//     wins[12] = new array[21,22,23,24,25,26,27],
+//     wins[13] = new array[28,29,30,31,32,33,34],
+//     wins[14] = new array[35,36,37,38,39,40,41],
+//
+//     //DIAGONAL
+//
+//     wins [15] = new array[35, 29, 23, 17, 11, 5],
+//     wins [16] = new array[28, 22, 16, 10, 4],
+//     wins [17] = new array[21, 15, 9, 3],
+//     wins [18] = new array[36, 30, 24, 18, 12, 6],
+//     wins [19] = new array[37, 31, 25, 19, 13],
+//     wins [20] = new array[38, 32, 26, 20],
+//
+//     //DIAGONAL2
+//
+//     wins [21] = new array[27, 19, 11, 3],
+//     wins [22] = new array[34, 26, 18, 10, 2],
+//     wins [23] = new array[41, 33, 25, 17, 9, 1],
+//     wins [24] = new array[40, 32, 24, 16, 8, 0],
+//     wins [25] = new array[39, 31, 23, 15, 7],
+//     wins [26] = new array[38, 30, 22, 14]
+//   ]
+//
+//
+// //function wins(array){
+// // switch (if (true) {
+// //
+// // }) {
+// //   case expression:
+// //
+// //     break;
+// //   default:
+// //
+// // }
+// //
+// // option()
+// //
+// // const winner={
+// //    for ( i = 0; i < 4.length; i++) {
+// //      4[i]
+// //    }
+// //   }
+//
+// //GRID
+// //   var createGrid = x
+// //   function createGrid(grid) {
+// //     for (let rows = 6; rows < x; rows++) {
+// //       for (let columns = 7; columns < x; columns++);
+// //     }
+// //   }
+// // }
+// //
+// //
+// // document.getElementById('createGrid').innerHTML = 'grid'
+// //   var i = 0;
+// //   while (i < array.length) {
+// //     text += array[i] + "<br>";
+// //     i++;'<br>'
+// //   }
+// //
+// // const winner={
+// //   // for ( i = 0; i < 4.length; i++) {
+// //   //   4[i]
+// //   // }
+// //   }
+// //   const mac = Math.floor(Math.random())
+// //
+// //   document.getElementById('winner').innerHTML = text
+// //   }
+// //
+// // const winner = function () {
+// //   if (human === [0][0],[0][1],[0][2],[0][3])
+// //     console.log('you win')
+// //     // else
+//
+// // const fix = function() {
+// //   for ([0][0] === 0)}
+// //     if ([0][1] === 0){
+// //       if ([0][2] === 0){
+// //         if ([0][3] === 0);
+// //       }
+// //     }
+// //   }
+// // }
+// //
+// //   // const winner = box[mac][human][fix]
+// //
+// // console.log(option)
+// //   //       for (human = 0; human < 6; human++);
+// //   //        for (mac = 0; mac < 7; mac++)
+// //   //
+// //   // {
+// //   //   function winner(){
+// //   //
+// //   // }
+// // // }
+// // document.getElementById('winner').innerHTML = fix[winner]
+// //PARA CAMBIAR EL COLO R DE LAS FICAHS
+//
+// // function changeColor(evt){
+// //   var clickedOn = evt.target;
+// //   // for HTML
+// //   clickedOn.style.backgroundColor = '#f00';
+// //
+// //   // for SVG
+// //   clickedOn.setAttribute('fill','red');
+// // }
+// // mySquare.addEventListener('click',changeColor,false);
+
+
+const checkWinOrLose( grid[][7], result, rowNum) {
+//  For checking whether any win or lose condition is reached. Returns 1 if win or lose is reached. else returns 0
+//  grid[][] is the 6X7 matrix
+//  result is the column number where the last coin was placed
+//  rowNum is the row number where the last coin was placed
+
+
+
+// Winning vertically
+wins[0]  = array(0, 7,  14, 21, 28, 35);
+wins[1]  = array(1, 8,  15, 22, 29, 36);
+wins[2]  = array(2, 9,  16, 23, 30, 37);
+wins[3]  = array(3, 10, 17, 24, 31, 38);
+wins[4]  = array(4, 11, 18, 25, 32, 39);
+wins[5]  = array(5, 12, 19, 26, 33, 40);
+wins[6]  = array(6, 13, 20, 27, 34, 41);
+// Winning horizontally
+wins[7]  = array(0,   1,  2,  3,  4,  5,  6);
+wins[8]  = array(7,   8,  9, 10, 11, 12, 13);
+wins[9]  = array(14, 15, 16, 17, 18, 19, 20);
+wins[10] = array(21, 22, 23, 24, 25, 26, 27);
+wins[11] = array(28, 29, 30, 31, 32, 33, 34);
+wins[12] = array(35, 36, 37, 38, 39, 40, 41);
+// Winning diagonally, left to right
+wins[13] = array(14, 22, 30, 38);
+wins[14] = array(7, 15, 23, 31, 39);
+wins[15] = array(0, 8, 16, 24, 32, 40);
+wins[16] = array(1, 9, 17, 25, 33, 41);
+wins[17] = array(2, 10, 18, 26, 34);
+wins[18] = array(3, 11, 19, 27);
+//Winning diagonally, right to left
+wins[19] = array(20, 26, 32, 38);
+wins[20] = array(13, 19, 25, 31, 37);
+wins[21] = array(6,  12, 18, 24, 30, 36);
+wins[22] = array(5,  11, 17, 23, 29, 35);
+wins[23] = array(4,  10, 16, 22, 28);
+wins[24] = array(3,   9, 15, 21);
